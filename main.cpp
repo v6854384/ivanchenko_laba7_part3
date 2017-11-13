12f7b62a8b3c38579c0c242f0e7feaaa7e385026
v6854384 <32385043+v6854384@users.noreply.github.com>
2017-11-13 13:29:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main (int argc, char* argv[])
 {
    string text = argv [1];
-   char symbhol = argv [2][0];
-   char symbhol2 = argv [3][0];
+   char symbol = argv [2][0];
+   char symbol2 = argv [3][0];
    for (int i=0; i<text.length(); i++)
       {
-         if(text[i]==symbhol)
-            text[i]=symbhol2
+         if(text[i]==symbol)
+           (text[i]=symbol2);
       }
    cout << text << endl;
 }
